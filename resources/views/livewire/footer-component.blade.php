87e249c076b0cda2f6b0ad1da9ca0fc967cbf222
VahagVag <harutyunyanvahag111@gmail.com>
2022-09-30 15:25:08 +0400

    Ecommerce commit

---
+++ resources/views/livewire/footer-component.blade.php
@@ -0,0 +1,108 @@
+<div>
+    <footer id="footer">
+        <div class="wrap-footer-content footer-style-1">
+
+            <div class="wrap-function-info">
+                <div class="container">
+                    <ul>
+                        <li class="fc-info-item">
+                            <i class="fa fa-truck" aria-hidden="true"></i>
+                            <div class="wrap-left-info">
+                                <h4 class="fc-name">Free Shipping</h4>
+                                <p class="fc-desc">Free On Oder Over $99</p>
+                            </div>
+
+                        </li>
+                        <li class="fc-info-item">
+                            <i class="fa fa-recycle" aria-hidden="true"></i>
+                            <div class="wrap-left-info">
+                                <h4 class="fc-name">Guarantee</h4>
+                                <p class="fc-desc">30 Days Money Back</p>
+                            </div>
+
+                        </li>
+                        <li class="fc-info-item">
+                            <i class="fa fa-credit-card-alt" aria-hidden="true"></i>
+                            <div class="wrap-left-info">
+                                <h4 class="fc-name">Safe Payment</h4>
+                                <p class="fc-desc">Safe your online payment</p>
+                            </div>
+
+                        </li>
+                        <li class="fc-info-item">
+                            <i class="fa fa-life-ring" aria-hidden="true"></i>
+                            <div class="wrap-left-info">
+                                <h4 class="fc-name">Online Suport</h4>
+                                <p class="fc-desc">We Have Support 24/7</p>
+                            </div>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!--End function info-->
+
+            <div class="main-footer-content">
+
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+                            <div class="wrap-footer-item">
+                                <h3 class="item-header">Contact Details</h3>
+                                <div class="item-content">
+                                    <div class="wrap-contact-detail">
+                                        <ul>
+                                            <li>
+                                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                                <p class="contact-txt">{{$setting->address}}</p>
+                                            </li>
+                                            <li>
+                                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                                <p class="contact-txt">{{$setting->phone}}</p>
+                                            </li>
+                                            <li>
+                                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                                <p class="contact-txt">{{$setting->email}}</p>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+
+                            <div class="wrap-footer-item">
+                                <h3 class="item-header">Hot Line</h3>
+                                <div class="item-content">
+                                    <div class="wrap-hotline-footer">
+                                        <span class="desc">Call Us toll Free</span>
+                                        <b class="phone-number">{{$setting->phone2}}</b>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="wrap-footer-item footer-item-second">
+                                <h3 class="item-header">Sign up for newsletter</h3>
+                                <div class="item-content">
+                                    <div class="wrap-newletter-footer">
+                                        <form action="#" class="frm-newletter" id="frm-newletter">
+                                            <input type="email" class="input-email" name="email" value="" placeholder="Enter your email address">
+                                            <button class="btn-submit">Subscribe</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+                </div>
+            </div>
+    </footer>
+
+</div>
